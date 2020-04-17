5b276afd8d257a30c3f93c2f198d15f964e3e365
Marvin <scutwqm@outlook.com>
2020-04-17 15:15:09 +0800

    add the folder named exercise-2.  have done the exercise

---
+++ exercise-2/ex2.py
@@ -0,0 +1,7 @@
+# _*_coding:utf-8- _*_
+# A comment, this is so you can read your program later.
+#Anything after the # is ignored by python.
+print("I could have code like this.")# and the comment after is ignored
+# You can alse use a comment to "disable" or comment out a piece of code:
+#print("This won't run.")
+print("This will run.")
