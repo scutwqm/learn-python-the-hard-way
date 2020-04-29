5ea895378d824fc6819d27d19fccbf50f0a5ef23
Marvin <scutwqm@outlook.com>
2020-04-29 22:01:41 +0800

    finish exercise18-20

--- exercise-20/ex20.py
+++ exercise-20/ex20.py
@@ -1,1 +1,34 @@
 # _*_ coding:utf-8- _*_
+from sys import argv
+
+scrpt, input_file = argv
+
+def print_all(f):
+    print(f.read())
+
+def rewind(f):
+    f.seek(0)
+
+def print_a_line(line_count, f):
+    print(line_count, f.readline())
+
+current_file = open(input_file, "r")
+
+print("First, let's print the whole file:\n")
+
+print_all(current_file)
+
+print("Now, let's rewind, kind of like a tape.")
+
+rewind(current_file)
+
+print("Let's print three lines:")
+
+current_line = 1
+print_a_line(current_line, current_file)
+
+current_line += 1
+print_a_line(current_line, current_file)
+
+current_line += 1
+print_a_line(current_line, current_file)
